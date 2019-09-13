dbac4e33029eeeaf56d08b69d31222e2a171a1bc
Timothy Beck <johndaveleet@gmail.com>
2019-09-13 01:42:14 -0600

    added a few functions that allow for the deletion of list items.

--- script.js
+++ script.js
@@ -10,22 +10,48 @@ function initShoppingList() {
 }
 
 function handleItemForm(event, formRef) {
-	if(event.preventDefault()) {
+	if(event.preventDefault) {
 		event.preventDefault();
 	}
 	addItemToShoppingList();
+	formRef.reset();
 	return false;
 }
 
 function addItemToShoppingList() {
 	let itemName = document.getElementById("item-name");
 	let amount = document.getElementById("item-amount");
-	let itemHtml = createListItemHtml(itemName.value, amount.value);
-	console.log("Item HTML: ", itemHtml)
+	let id = getRandomInt(0, 10000000);
+
+	//Creates list item and appends to page.
+	let itemHtml = createListItemHtml(itemName.value, amount.value, id);
 	let itemListRef = document.getElementById("shopping-list");
 	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+	setDeleteButtonEvent(id);
 }
 
-function createListItemHtml (itemName, amount) {
-	return `<li>${itemName} - ${amount}</li>`
+function setDeleteButtonEvent(id) {
+	let deleteButton = document.getElementById("button"+id);
+	deleteButton.addEventListener("click", () => {
+		removeListItem(id);
+	});
+}
+
+function createListItemHtml (itemName, amount, id) {
+	return `<li id="item${id}">
+				${itemName} - ${amount}
+				<button id="button${id}" type="button">Delete  Item</button>
+			</li>`
+}
+
+function removeListItem(id) {
+	let listItem = document.getElementById("item"+id);
+	listItem.parentNode.removeChild(listItem);
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
 }
